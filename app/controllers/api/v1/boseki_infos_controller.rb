25ab37708284956739b2d19b11b4cb4605977d35
KeisukeKitazumi <kitazumi.gc8@gmail.com>
2023-04-01 22:21:54 +0900

    update用のparamsを切り出し

--- app/controllers/api/v1/boseki_infos_controller.rb
+++ app/controllers/api/v1/boseki_infos_controller.rb
@@ -18,24 +18,15 @@ class Api::V1::BosekiInfosController < Api::V1::ApplicationController
     render json: { status: 500, error: "Failure: #{e}" }
   end
 
-  # rubocop:disable Metrics/AbcSize
   def update
     # curl -X PATCH -H 'Content-Type: application/json' -d '{"data": {"record_id": "245444", "cemetery_name": "本能寺" }}' "http://localhost:8000/api/v1/boseki_info"
     data = Soukyakukanri.find(boseki_info_params[:record_id])
-    data.update(
-      cemetery_name: boseki_info_params[:cemetery_name] || data.cemetery_name,
-      cemetery_addr: boseki_info_params[:cemetery_addr] || data.cemetery_addr,
-      contact_time: boseki_info_params[:addr] || data.contact_time,
-      contact_note: boseki_info_params[:area] || data.contact_note,
-      customer_request: boseki_info_params[:mitsumori] || data.customer_request,
-      email: boseki_info_params[:email] || data.email
-    )
+    data.update(update_params(data))
     render json: { status: :ok, record_id: data.record_id }
   rescue StandardError => e
     Rails.logger.error e
     render json: { status: 500, error: "Failure: #{e}" }
   end
-  # rubocop:enable Metrics/AbcSize
 
   private
 
@@ -43,4 +34,17 @@ class Api::V1::BosekiInfosController < Api::V1::ApplicationController
     params.require(:data).permit(:record_id, :name, :tel, :work_type, :cemetery_name, :cemetery_addr, :addr, :area,
                                  :mitsumori, :email)
   end
+
+  # rubocop:disable Metrics/AbcSize
+  def update_params(data)
+    {
+      cemetery_name: boseki_info_params[:cemetery_name] || data.cemetery_name,
+      cemetery_addr: boseki_info_params[:cemetery_addr] || data.cemetery_addr,
+      contact_time: boseki_info_params[:addr] || data.contact_time,
+      contact_note: boseki_info_params[:area] || data.contact_note,
+      customer_request: boseki_info_params[:mitsumori] || data.customer_request,
+      email: boseki_info_params[:email] || data.email
+    }
+  end
+  # rubocop:enable Metrics/AbcSize
 end
