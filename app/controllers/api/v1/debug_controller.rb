628c06c298ee2f0e1fa850119b2f3e289d9a4786
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-04-08 22:23:35 +0900

    fix

--- app/controllers/api/v1/debug_controller.rb
+++ app/controllers/api/v1/debug_controller.rb
@@ -1,6 +1,6 @@
 #  TODO: Remove this controller before production
 class Api::V1::DebugController < Api::V1::ApplicationController
   def show_env
-    render json: { redis_host: ENV['REDIS_HOST'], redis_port: ENV['REDIS_PORT'], rails_env: ENV['RAILS_ENV'] }
+    render json: { redis_host: ENV['REDIS_HOST'], redis_port: ENV['REDIS_PORT'], rails_env: ENV['RAILS_ENV'], redis_url: ENV['REDIS_URL'] }
   end
 end
