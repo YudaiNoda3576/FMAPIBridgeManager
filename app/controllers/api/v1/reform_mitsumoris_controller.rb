ce7ba939444bb57e8043ad7f45485744cf556fe8
ttakuma17 <ttanimiya@gmail.com>
2023-04-12 23:34:21 +0900

    updateメソッドのパラメータ確認

--- app/controllers/api/v1/reform_mitsumoris_controller.rb
+++ app/controllers/api/v1/reform_mitsumoris_controller.rb
@@ -21,7 +21,9 @@ class Api::V1::ReformMitsumorisController < Api::V1::ApplicationController
   def update   
     # curl -X PATCH -H 'Content-Type: application/json' -d '{"data": {"record_id": "245551", "budget": "30万円以内" }}' "http://localhost:8000/api/v1/reform_mitsumori"
     # ページ遷移するごとにキー名が変わっていきます(7ステップ)、budget, building_type, point[], building_age, [contact_time[]　, contact_remark], email, mitsumori
+    # binding.pry
     data = Soukyakukanri.find(reform_mitsumori_params[:record_id])
+    binding.pry
     data.update(update_params(data))
     render json: { status: :ok, record_id: data.record_id }
   rescue StandardError => e
@@ -32,21 +34,28 @@ class Api::V1::ReformMitsumorisController < Api::V1::ApplicationController
   private
 
   def reform_mitsumori_params
-    params.require(:data).permit(:record_id, :pref, :city, :cho, :addr, :name, :tel, :building_type, {point: []}, {contact_time: []}, :contact_remark, :budget, :building_age, :building_age, :email, :mitsumori)
+    params.require(:data).permit(:record_id, :pref, :city, :cho, :addr, :name, :tel, :building_type, :contact_remark, :budget, :building_age, :building_age, :email, :mitsumori, point: [], contact_time: [])
+    # params.require(:data).permit(:record_id, point: [])
   end
 
   def update_params(data)
     {
-      budget: reform_mitsumori_params[:budget] || data.budget, # FM上にカラムないです
+      budget: reform_mitsumori_params[:budget] || data.budget,
       building_type: reform_mitsumori_params[:building_type] || data.building_type,
-      construction_type: reform_mitsumori_params[:point[]] || data.construction_type,
+      construction_type: reform_mitsumori_params[:point].join(",") || data.construction_type,
       building_age: reform_mitsumori_params[:building_age] || data.building_age, 
-      contact_time: reform_mitsumori_params[:contact_time] || data.contact_time,
+      contact_time: convert_array_to_string(reform_mitsumori_params[:contact_time]) || data.contact_time,
       contact_note: reform_mitsumori_params[:contact_remark] || data.contact_note,
       email: reform_mitsumori_params[:email] || data.email,
-      customer_request: reform_mitsumori_params[:mitsumori] || data.customer_request,
+      customer_request: reform_mitsumori_params[:mitsumori] || data.customer_request
     }
   end
+
+  def convert_array_to_string(array)
+    # binding.pry
+    array.join(",")
+  end
+
 end
 
     # 以下のいずれかなら status はOKになる　DBへは登録されず
