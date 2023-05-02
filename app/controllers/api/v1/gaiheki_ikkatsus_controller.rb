e231ef369fa9a9a458d9e78f1316f2f6c8114b8d
KeisukeKitazumi <kitazumi.gc8@gmail.com>
2023-05-02 14:43:27 +0900

    updateアクション追加

--- app/controllers/api/v1/gaiheki_ikkatsus_controller.rb
+++ app/controllers/api/v1/gaiheki_ikkatsus_controller.rb
@@ -18,9 +18,32 @@ class Api::V1::GaihekiIkkatsusController < Api::V1::ApplicationController
     render json: { status: 500, error: "Failure: #{e}" }
   end
 
+  def update
+    # curl -X PATCH -H 'Content-Type: application/json' -d '{"data": {"record_id": "176282", "building_type": "マンション" }}' "http://localhost:8000/api/v1/gaiheki_ikkatsu"
+    # ページ遷移するごとにキー名が変わっていきます、building_type, position, work_date, [contact_time, contact_remark], email, mitsumori
+    data = SunlifeSoukyakukanri.find(gaiheki_ikkatsu_params[:record_id])
+    data.update(update_params(data))
+    render json: { status: :ok, record_id: data.record_id }
+  rescue StandardError => e
+    Rails.logger.error e
+    render json: { status: 500, error: "Failure: #{e}" }
+  end
+
   private
 
   def gaiheki_ikkatsu_params
     params.require(:data).permit(:record_id, :addr, :name, :tel, :building_type, :position, :work_date, :contact_time, :contact_remark, :email, :mitsumori)
+  end
+
+  def update_params(data)
+    {
+      building_type: gaiheki_ikkatsu_params[:building_type] || data.building_type,
+      construction_type: gaiheki_ikkatsu_params[:position] || data.construction_type,
+      work_date: gaiheki_ikkatsu_params[:work_date] || data.work_date,
+      contact_time: gaiheki_ikkatsu_params[:contact_time] || data.contact_time,
+      contact_note: gaiheki_ikkatsu_params[:contact_remark] || data.contact_note,
+      email: gaiheki_ikkatsu_params[:email] || data.email,
+      customer_request: gaiheki_ikkatsu_params[:mitsumori] || data.customer_request
+    }
   end
 end
