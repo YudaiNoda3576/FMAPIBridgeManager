5e4759e80fcfd6f38059ea57e038e8cf4ad07731
KeisukeKitazumi <kitazumi.gc8@gmail.com>
2023-06-13 21:24:28 +0900

    もう1つのフォームが存在していたので対応

--- app/controllers/api/v1/kaitai_hikakus_controller.rb
+++ app/controllers/api/v1/kaitai_hikakus_controller.rb
@@ -22,10 +22,33 @@ class Api::V1::KaitaiHikakusController < Api::V1::ApplicationController
     render json: { status: 500, error: "Failure: #{e}" }
   end
 
+  def update
+    # curl -X PATCH -H 'Content-Type: application/json' -d '{"data": {"record_id": "176297", "work_date": "できるだけ早く" }}' "http://localhost:8000/api/v1/kaitai_hikaku"
+    # ページ遷移するごとにキー名が変わっていきます、area, work_date, [contact_time, contact_remark], email, 'mitsumori'
+    data = SunlifeSoukyakukanri.find(kaitai_hikaku_params[:record_id])
+    data.update(update_params(data))
+    render json: { status: :ok, record_id: data.record_id }
+  rescue StandardError => e
+    Rails.logger.error e
+    render json: { status: 500, error: "Failure: #{e}" }
+  end
+
+
   private
 
   def kaitai_hikaku_params
-    params.require(:data).permit(:record_id, :building_type, :addr, :name, :tel,
-                                 :mitsumori)
+    params.require(:data).permit(:record_id, :position, :building_type, :addr, :name, :tel, :area, :work_date,
+                                 :contact_time, :contact_remark, :email, :mitsumori)
+  end
+
+  def update_params(data)
+    { 
+      area: kaitai_hikaku_params[:area] || data.area,
+      work_date: kaitai_hikaku_params[:work_date] || data.work_date,
+      contact_time: kaitai_hikaku_params[:contact_time] || data.contact_time,
+      contact_note: kaitai_hikaku_params[:contact_remark] || data.contact_note,
+      email: kaitai_hikaku_params[:email] || data.email,
+      customer_request: kaitai_hikaku_params[:mitsumori] || data.customer_request
+    }
   end
 end
